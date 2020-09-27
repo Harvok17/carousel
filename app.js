37f16e4f74ee1d3782b8e072c58f0523259052a8
Harvey Granfil <jhgranfil@gmail.com>
2020-09-27 16:45:42 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+let x = 0;
+const arr = ['a','b','c','d','e'];
+const back = document.getElementById('back');
+const forward = document.getElementById('forward');
+const carousel = document.getElementById('carousel');
+const circles = document.querySelector('.circles');
+
+
+
+carousel.addEventListener('click',rotate);
+
+circles.addEventListener('click',whiten);
+
+
+function rotate(choice){
+  
+    if (choice.target === forward){
+    reset();
+    x += 1;
+    if(x >= arr.length){
+      x = 0;
+      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+      document.getElementById(`${arr[x]}`).style.background = 'white';
+    }
+      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+      document.getElementById(`${arr[x]}`).style.background = 'white';
+
+
+  } else if (choice.target === back){
+    reset();
+    x -= 1;
+    if(x < 0){
+      x = arr.length - 1;
+      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+      document.getElementById(`${arr[x]}`).style.background = 'white';
+
+
+    }
+      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+      document.getElementById(`${arr[x]}`).style.background = 'white';
+  }
+}
+
+function whiten(e){
+  if(e.target.id === 'a' || e.target.id === 'b'||e.target.id === 'c'||e.target.id === 'd'||e.target.id === 'e'){
+    reset();
+    x = arr.indexOf(e.target.id);
+    carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+    
+  e.target.style.background = 'white';
+  }
+}
+
+function reset(){
+  let list = circles.children;
+    for (let i = 0; i < list.length; i++) {
+      list[i].style.background = 'gray';
+    }
+}
