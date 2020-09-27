af19f0972fdd78bd555e605fa06438166fe4a9a0
Harvey Granfil <jhgranfil@gmail.com>
2020-09-27 19:46:09 +0800

    Update commit

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
 let x = 0;
-const arr = ['a','b','c','d','e'];
-const back = document.getElementById('back');
-const forward = document.getElementById('forward');
-const carousel = document.getElementById('carousel');
-const circles = document.querySelector('.circles');
+const arr = ["a", "b", "c", "d", "e"];
+const back = document.getElementById("back");
+const forward = document.getElementById("forward");
+const carousel = document.getElementById("carousel");
+const circles = document.querySelector(".circles");
 
+carousel.addEventListener("click", rotate);
 
+circles.addEventListener("click", whiten);
 
-carousel.addEventListener('click',rotate);
-
-circles.addEventListener('click',whiten);
-
-
-function rotate(choice){
-  
-    if (choice.target === forward){
+function rotate(choice) {
+  if (choice.target === forward) {
     reset();
     x += 1;
-    if(x >= arr.length){
+    if (x >= arr.length) {
       x = 0;
       carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
-      document.getElementById(`${arr[x]}`).style.background = 'white';
+      document.getElementById(`${arr[x]}`).style.background = "white";
     }
-      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
-      document.getElementById(`${arr[x]}`).style.background = 'white';
-
-
-  } else if (choice.target === back){
+    carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+    document.getElementById(`${arr[x]}`).style.background = "white";
+  } else if (choice.target === back) {
     reset();
     x -= 1;
-    if(x < 0){
+    if (x < 0) {
       x = arr.length - 1;
       carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
-      document.getElementById(`${arr[x]}`).style.background = 'white';
-
-
+      document.getElementById(`${arr[x]}`).style.background = "white";
     }
-      carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
-      document.getElementById(`${arr[x]}`).style.background = 'white';
+    carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
+    document.getElementById(`${arr[x]}`).style.background = "white";
   }
 }
 
-function whiten(e){
-  if(e.target.id === 'a' || e.target.id === 'b'||e.target.id === 'c'||e.target.id === 'd'||e.target.id === 'e'){
+function whiten(e) {
+  if (
+    e.target.id === arr[0] ||
+    e.target.id === arr[1] ||
+    e.target.id === arr[2] ||
+    e.target.id === arr[3] ||
+    e.target.id === arr[4]
+  ) {
     reset();
     x = arr.indexOf(e.target.id);
     carousel.style.backgroundImage = `url(/image/${arr[x]}.jpg)`;
-    
-  e.target.style.background = 'white';
+
+    e.target.style.background = "white";
   }
 }
 
-function reset(){
+function reset() {
   let list = circles.children;
-    for (let i = 0; i < list.length; i++) {
-      list[i].style.background = 'gray';
-    }
+  for (let i = 0; i < list.length; i++) {
+    list[i].style.background = "gray";
+  }
 }
